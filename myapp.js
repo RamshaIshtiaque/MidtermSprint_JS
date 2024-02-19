8657a54e1fbb2be4df700729fa3fa0e2e55ee94b
RamshaIshtiaque <ramshaishtiaque@gmail.com>
2024-02-18 22:10:03 -0330

    added the config files

--- myapp.js
+++ myapp.js
@@ -1,6 +1,7 @@
 global.DEBUG = true;
 
 const fs = require ('fs');
+const { initializeApp } = require('./init.js');
 
 const myArgs = process.argv.slice(2);
 if(DEBUG) if(myArgs.length >= 1 ) console.log('the myapp.args: ' , myArgs);
@@ -9,6 +10,7 @@ switch (myArgs[0]) {
   case 'init':
   case 'i':
       if(DEBUG) console.log(myArgs[0], ' - initialize the app.');
+      initializeApp();
       break;
   case 'config':
   case 'c':
