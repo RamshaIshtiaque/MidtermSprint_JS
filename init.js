c033f2c0827abebd91176aa8a95e232a1e8b3b09
RamshaIshtiaque <ramshaishtiaque@gmail.com>
2024-02-20 22:05:41 -0330

    set reset config

--- init.js
+++ init.js
@@ -1,26 +1,8 @@
 const fs = require("fs");
 const path = require('path');
 const fsPromises = require('fs').promises;
+const { folders, configjson, tokenjson } = require('./templates');
 
-const folders = ['models', 'views', 'routes', 'logs', 'json'];
-const configjson = { 
-    name: 'AppConfigCLI',
-    version: '1.0.0',
-    description: 'The Command Line Interface (CLI) for the MyApp.',
-    main: 'myapp.js',
-    superuser: 'adm1n',
-    database: 'exampledb'
-};
-
-const tokenjson = [{
-   created: '1969-01-31 12:30:00',
-   username: 'username',
-   email: 'user@example.com',
-   phone: '5556597890',
-   token: 'token',
-   expires: '1969-02-03 12:30:00',
-   confirmed: 'tbd'
-}];
 
 function createFolders() {
     if(DEBUG) console.log('init.createFolders()');
