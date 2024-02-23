c48f9db58404d70e1da9c65403d521e8e428cd7b
RamshaIshtiaque <ramshaishtiaque@gmail.com>
2024-02-22 23:01:01 -0330

    completed tokens

--- myapp.js
+++ myapp.js
@@ -3,6 +3,7 @@ global.DEBUG = true;
 const fs = require ('fs');
 const { initializeApp } = require('./init.js');
 const { configApp } = require('./config.js');
+const { tokenApp } = require('./token.js');
 
 const myArgs = process.argv.slice(2);
 if(DEBUG) if(myArgs.length >= 1 ) console.log('the myapp.args: ' , myArgs);
@@ -21,6 +22,7 @@ switch (myArgs[0]) {
   case 'token':
   case 't':
       if(DEBUG) console.log(myArgs[0], ' - generate a user token');
+      tokenApp();
       break;  
   case '--help':
   case '--h':
