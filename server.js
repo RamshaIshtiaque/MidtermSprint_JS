1301a19ab181bfb386f78c751295673711b3f4ee
RamshaIshtiaque <ramshaishtiaque@gmail.com>
2024-02-23 12:48:16 -0330

    completed the web form

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const path = require("path");
+const bodyParser = require('body-parser');
+const { v4: uuidv4 } = require('uuid');
+
+const app = express();
+const PORT = 3000;
+
+// Serve static files from the public directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Parse URL-encoded bodies (as sent by HTML forms)
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (request, response) => {
+    console.log("root route.");
+    response.send("the route for the site's root /.");
+});
+
+app.get('/html', (request, response) => {
+    const filePath = path.join(__dirname, 'views/index.html');
+    response.sendFile(filePath);
+});
+
+app.post('/generateToken', (req, res) => {
+    const username = req.body.username;
+    const token = generateToken(username);
+    res.send(`Token generated for ${username}: ${token}`);
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
+
+function generateToken(username) {
+    // Generate a token using uuid
+    const token = uuidv4();
+    return token;
+}
+
